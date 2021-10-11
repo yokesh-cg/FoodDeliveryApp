94b475d807be2dd91f4b43dbb42e2cf2079f9dba
yokesh-cg <89562237+yokesh-cg@users.noreply.github.com>
2021-10-11 10:49:24 +0530

    Delete fooddeliveryapp/fooddeliveryapp directory

--- fooddeliveryapp/fooddeliveryapp/src/main/java/com/cg/project/fooddeliveryapp/service/ILoginService.java
+++
@@ -1,9 +0,0 @@
-package com.cg.project.fooddeliveryapp.service;
-
-import com.cg.project.fooddeliveryapp.entity.Login;
-
-public interface ILoginService {
-	public Login signIn(Login login);
-	public Login signOut(Login login);
-
-}
